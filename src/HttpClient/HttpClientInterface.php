42ba8030958e37bf26629e43a23f52e2dc6ac110
Tony Roy <tonyroy@wcksoft.com>
2021-03-14 10:13:26 -0300

    Create HttpClientInterface.php

---
+++ src/HttpClient/HttpClientInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace wickedsoft\Virtualizor\HttpClient;
+
+interface HttpClientInterface
+{
+    /**
+     * @return array
+     */
+    public function getOptions();
+
+    /**
+     * @param array $body
+     * @return mixed
+     */
+    public function get($path="", $query=[]);
+
+    /**
+     * @param array $body
+     * @return mixed
+     */
+    public function post($path="", $body=[]);
+
+    /**
+     * @param array $body
+     * @return mixed
+     */
+    public function put($path="", $body=[]);
+
+    /**
+     * @param array $body
+     * @return mixed
+     */
+    public function delete($path="", $body=[]);
+
+    /**
+     * @param array $options
+     */
+    public function setOptions(array $options);
+}
